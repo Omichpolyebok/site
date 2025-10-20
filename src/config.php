f89e1592406f7ec1aec947a48a9f5fc071bb1156
Omichpolyebok <184048278+Omichpolyebok@users.noreply.github.com>
2025-10-20 15:07:12 +0500

    Add SMTP configuration in config.php

---
+++ src/config.php
@@ -0,0 +1,9 @@
+<?php
+return [
+  'smtp_host'   => 'smtp.yandex.ru',
+  'smtp_user'   => 'yourmail',
+  'smtp_pass'   => 'yoursmtppass(need to get it in settings of yandex. NOT USER PASS)',
+  'smtp_port'   => 465,               // или 587, см. ниже
+  'from_email'  => 'yourmail',
+  'from_name'   => 'MySite',
+];
