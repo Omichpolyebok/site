9377f3c9c9aab73bf0790a02116faa69a68735fa
Omich <184048278+Omichpolyebok@users.noreply.github.com>
2025-12-05 15:07:02 +0300

    forgor files

---
+++ inc/header.php
@@ -0,0 +1,13 @@
+<header>
+  <nav class="navbar">
+    <a href="index.php">Главная</a>
+    <?php if (!empty($_SESSION['user_id'])): ?>
+      <a href="logout.php">Выйти</a>
+    <?php else: ?>
+      <a href="login.php">Войти</a>
+      <a href="register.php">Регистрация</a>
+    <?php endif; ?>
+  </nav>
+  <hr>
+</header>
+
